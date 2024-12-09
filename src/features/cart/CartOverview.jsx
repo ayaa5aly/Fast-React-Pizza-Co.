19ededb7d458d65e60c415e02858f9af7ec0536e
ayaa5aly <ayaaaly512@gmail.com>
2024-12-09 21:23:35 +0200

    first commit

---
+++ src/features/cart/CartOverview.jsx
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { getTotalCartPrice, getTotalCartQuantity } from "./cartSlice";
+import { formatCurrency } from "../../utils/helpers";
+
+function CartOverview() {
+  const totalCartQuantity = useSelector(getTotalCartQuantity );
+  const totalCartPrice = useSelector(getTotalCartPrice);
+
+if(!totalCartQuantity) return null;
+
+  return (
+    <div className="bg-stone-800 uppercase
+     text-stone-200 px-4 py-4 sm:px-6 text-sm md:text-base flex item-center justify-between ">
+      <p className="space-x-4 text-stone-300 font-semibold sm:space-x-6">
+        <span>{totalCartQuantity} pizzas</span>
+        <span>{formatCurrency(totalCartPrice)}</span>
+      </p>
+      <Link to="/cart">Open cart &rarr;</Link>
+    </div>
+  );
+}
+
+export default CartOverview;
